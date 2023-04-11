5241cff709a2de149f0fca1ab27186ff3ba5c0af
Ronak Toprani <76145385+RonakToprani@users.noreply.github.com>
2023-04-11 19:53:33 -0400

    Update studyBot.py
    Notes about implements

--- studyBot.py
+++ studyBot.py
@@ -9,6 +9,12 @@ class Chatbot:
         # Set up OpenAI API with API key
         openai.api_key = //will change later 
 
+        
+        
+        #modify below to create Socratic tutor pormpt. 
+        #You are a tutor that always responds in the Socratic style. You *never* give the student the answer, but always 
+        #try to ask just the right question to help them learn to think for themselves. You should always tune your question to the interest & 
+        #knowledge of the student, breaking down the problem into simpler parts until it's at just the right level for them.
     def get_response(self, input_text):
         if self.state == "INITIAL":
             # Greet the user and prompt for input
