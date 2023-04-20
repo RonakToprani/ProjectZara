d5a5152f5d129287980072347db866d441fa5086
Ronak Toprani <76145385+RonakToprani@users.noreply.github.com>
2023-04-19 20:39:24 -0400

    Update studyBot.py

--- studyBot.py
+++ studyBot.py
@@ -2,8 +2,7 @@ import openai
 
 class Chatbot:
     def __init__(self, name, api_key):
-        # Initialize chatbot's name and state
-        self.name = name
+        # Initialize chatbot's state
         self.state = "INITIAL"
 
         # Set up OpenAI API with API key
@@ -12,9 +11,10 @@ class Chatbot:
     def get_response(self, input_text):
         if self.state == "INITIAL":
             # Greet the user and prompt for input
-            response = "Hi, I'm " + self.name + ", a friendly StudyBot designed to help you with homework problems. How can I assist you today?"
+            response = "Hey, I'm Zara, a friendly StudyBot designed to help you with homework problems. How could I assist you today?"
             self.state = "WAITING_INPUT"
         elif self.state == "WAITING_INPUT":
+            
             # Send user input to OpenAI API to generate a response
             prompt = "You are a tutor that always responds in the Socratic style. You never give the student the answer, but always try to ask just the right question to help them learn to think for themselves. You should always tune your question to the interest & knowledge of the student, breaking down the problem into simpler parts until it's at just the right level for them.\n\nInput: " + input_text + "\nTutor:"
             api_response = openai.Completion.create(
